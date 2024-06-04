3257ee65dc1d79b64a9ba1f30a5eb3d4802fc001
debil <qwe@example.ru>
2024-06-04 09:51:36 +0300

    13

--- tg_insert_data.py
+++ tg_insert_data.py
@@ -29,4 +29,4 @@ async def cmd_start(message: types.Message):
 
 @tg_bot.message(F.text.lower() == "создать напоминание")
 async def create_notification(message: types.Message):
-    await message.answer("Проверка actions 8", reply_markup=start_keyboard)
+    await message.answer("Проверка actions 9", reply_markup=start_keyboard)
